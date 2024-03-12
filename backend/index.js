f235f440480f15547941b3ce62fbf164267ed5ae
Andrew Diaz <andrewdiazn20@gmail.com>
2024-03-12 14:49:53 -0700

    5changes

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,9 @@ app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use('/places', require('./controllers/places'))
+app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
 
 // Controllers & Routes
 
@@ -22,3 +25,4 @@ app.use('/users', require('./controllers/users'))
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`)
 })
+
